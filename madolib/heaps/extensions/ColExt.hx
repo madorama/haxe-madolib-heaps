b9459db8a30fd7508ecf3346531bf2c94c9dbd19
madorama <madorama999@gmail.com>
2024-02-22 21:42:07 +0900

    🏗️ Change: Colliderを返すように

--- madolib/heaps/extensions/ColExt.hx
+++ madolib/heaps/extensions/ColExt.hx
@@ -2,11 +2,12 @@ package madolib.heaps.extensions;
 
 import madolib.geom.Bounds;
 import madolib.heaps.node.BoxCollider;
+import madolib.heaps.node.Collider;
 
 class ColExt {
-    public inline static function createBoxCollider(self: Bounds): BoxCollider
+    public inline static function createBoxCollider(self: Bounds): Collider
         return new BoxCollider(self.left, self.top, self.width, self.height);
 
-    public inline static function createBoxColliders(self: Array<Bounds>): Array<BoxCollider>
+    public inline static function createBoxColliders(self: Array<Bounds>): Array<Collider>
         return self.map(createBoxCollider);
 }
